8d9d1d789bdd0016495d4004772a2f93392cce03
Hussain Wali <13bscshwali@seecs.edu.pk>
2024-08-06 14:22:02 +0500

    F

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,6 +2,10 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 let db: any = null;
+console.log('-----------------------------------------------------');
+console.log('process.env.DATABASE_URL',process.env.DATABASE_URL);
+console.log('-----------------------------------------------------');
+
 if (process.env.DATABASE_URL) {
   const sql = neon(process.env.DATABASE_URL! || "-32");
 
