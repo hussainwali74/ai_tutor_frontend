56423db5bad821b69a49c3ca9c5d1f405e196503
Hussain Wali <13bscshwali@seecs.edu.pk>
2024-08-06 16:43:34 +0500

    J

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,14 +2,15 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-console.log('-----------------------------------------------------');
-console.log('process.env',process.env);
+let db:any;
 console.log('-----------------------------------------------------');
 console.log('process.env.NEXT_PUBLIC_DATABASE_URL',process.env.NEXT_PUBLIC_DATABASE_URL);
 console.log('-----------------------------------------------------');
 
-const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL||'s');
+if (process.env.NEXT_PUBLIC_DATABASE_URL){
+    const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL||'s');
+    db = drizzle(sql, { schema });
+}
 
-const db = drizzle(sql, { schema });
 
 export default db;
