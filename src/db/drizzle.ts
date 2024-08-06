a08df1d7a207f82508d40118352992bc8efea55d
Hussain Wali <13bscshwali@seecs.edu.pk>
2024-08-06 15:25:03 +0500

    tempurl

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,7 +2,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL! || "-32");
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL! || "postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-gross-2222that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require");
 
 const db = drizzle(sql, { schema });
 
