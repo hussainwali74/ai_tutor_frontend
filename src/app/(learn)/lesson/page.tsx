6d1fd37a98e7d5f39706af49f1755bd4ca7a6584
Hussain Wali <13bscshwali@seecs.edu.pk>
2024-08-06 15:36:29 +0500

    neon maaf karde bhai

--- src/app/(learn)/lesson/page.tsx
+++ src/app/(learn)/lesson/page.tsx
@@ -9,29 +9,29 @@ import LessonCardComponent from "./lesson_components/lesson_card.component";
 import { getLessons } from "@/db/queries/lesson.queries";
 
 export default function Page() {
-    
   const [lessons, setLessons] = useState<any[]>([]);
 
   const [startedLessonId, setStartedLessonId] = useState<number | undefined>();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
- 
+
   useEffect(() => {
     async function getAllLessons() {
       try {
-        console.log('------------------')
-        const response = await getLessons()
-        console.log('-----------------------------------------------------');
-        console.log('response',response);
-        console.log('-----------------------------------------------------');
-        if (response){
+        console.log("-----------------------------------------------------");
+        console.log("on frontend process.env.NEXT_PUBLIC_DATABASE_URL", process.env.NEXT_PUBLIC_DATABASE_URL);
+        console.log("-----------------------------------------------------");
+
+        const response = await getLessons();
+        console.log("-----------------------------------------------------");
+        console.log("response", response);
+        console.log("-----------------------------------------------------");
+        if (response) {
           setLessons(response);
         }
       } catch (error) {
         console.log("lesson page: get all lessons error 30", error);
-        console.log(
-          "========================================================="
-        );
+        console.log("=========================================================");
       }
     }
     getAllLessons();
@@ -53,15 +53,12 @@ export default function Page() {
       <div className="grid grid-cols-2 gap-2 lg:grid-cols-4 lg:gap-6 ">
         {lessons?.length
           ? lessons?.map((lesson, i) => (
-            <LessonCardComponent key={i} i={i} lesson={lesson} startLesson={startLesson} />
+              <LessonCardComponent key={i} i={i} lesson={lesson} startLesson={startLesson} />
             ))
           : null}
       </div>
       {isModalOpen && (
-        <Modal
-          isOpen={isModalOpen}
-          handleClose={() => setIsModalOpen(!isModalOpen)}
-        >
+        <Modal isOpen={isModalOpen} handleClose={() => setIsModalOpen(!isModalOpen)}>
           <div className="flex flex-col w-full h-full">
             <Image
               src={"/lesson_modal_heading.png"}
@@ -77,10 +74,7 @@ export default function Page() {
 
                 <div className="flex flex-col justify-center gap-8 py-4 bg-full sm:flex-row align-center">
                   <Link
-                    href={
-                      "/home?id=" +
-                      lessons.find((e) => e.id === startedLessonId)?.id
-                    }
+                    href={"/home?id=" + lessons.find((e) => e.id === startedLessonId)?.id}
                     className="inline-flex w-full  justify-center  px-3 py-2 text-sm font-medium  rounded-2xl text-white bg-gradient-to-r from-[#FFCB17] to-[#FBB21A]  focus:ring-4 focus:outline-none focus:ring-blue-300 "
                   >
                     Proceed to Lesson
--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,6 +2,10 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
+console.log('-----------------------------------------------------');
+console.log('process.env.NEXT_PUBLIC_DATABASE_URL',process.env.NEXT_PUBLIC_DATABASE_URL);
+console.log('-----------------------------------------------------');
+
 const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL! || "postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-gross-2222that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require");
 
 const db = drizzle(sql, { schema });
