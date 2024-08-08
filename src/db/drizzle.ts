59e51aae7e13c107c15dc1465fd521bf823d88fc
Hussain Wali <13bscshwali@seecs.edu.pk>
2024-08-08 12:19:24 +0500

    q8

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -6,8 +6,8 @@ import * as schema from "./schema";
 //   "postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-frost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require" ||
 //     "postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-drost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require"
 // );
-// const sql = neon(process.env.DATABASE_URL||'postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-drost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require');
-const sql = neon(process.env.DATABASE_URL!);
+// const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL||'postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-drost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require');
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL!);
 
 const db = drizzle(sql, { schema });
 
