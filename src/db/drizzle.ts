fb3ccdfa152ef33c59b7f1ba905fca6fe9a2153c
Hussain Wali <13bscshwali@seecs.edu.pk>
2024-08-08 12:50:37 +0500

    q12

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,19 +1,14 @@
-
-import { cwd } from 'node:process';
 import { neon } from "@neondatabase/serverless";
-import { loadEnvConfig } from '@next/env';
-loadEnvConfig(cwd());
-
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
 // const sql = neon(
-//   "postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-frost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require" ||
-//     "postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-drost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require"
-// );
-// const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL||'postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-drost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require');
-// const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL||"postgresql://<user>:<password>@<endpoint_hostname>.neon.tech:<port>/<dbname>?sslmode=require");
-const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL as string);
+  //   "postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-frost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require" ||
+  //     "postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-drost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require"
+  // );
+  // const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL||'postgresql://aitutordb_owner:x3TkyiNJCGV2@ep-tight-drost-a1j1that.ap-southeast-1.aws.neon.tech/aitutordb?sslmode=require');
+  const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL||"postgresql://<user>:<password>@<endpoint_hostname>.neon.tech:<port>/<dbname>?sslmode=require");
+
 
 const db = drizzle(sql, { schema });
 
